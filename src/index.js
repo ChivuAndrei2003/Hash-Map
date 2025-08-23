d2f335628a38018318ebf4bfb20aafd4003f5923
ChivuAndrei2003 <chivuandrei2003@gmail.com>
2025-08-23 16:24:28 +0300

    added set() and resize() methods

--- src/index.js
+++ src/index.js
@@ -1,27 +1,63 @@
 class HashMap {
-  constructor(load_factor = 0.75, capacity = 16) {
-    this.load_factor = load_factor;
-      this.capacity = capacity;
-      this.buckets = new Array(capacity);
-      this.size = 0;
+  constructor(capacity = 16, loadFactor = 0.75) {
+    this.loadFactor = loadFactor;
+    this.capacity = capacity;
+    this.buckets = new Array(capacity);
+    this.size = 0;
   }
   hash(key) {
     let hashCode = 0;
     const primeNumber = 31;
 
     for (let i = 0; i < key.length; i++) {
-      hashCode = ( primeNumber * hashCode + key.charCodeAt(i) % this.capacity ) ;
+      hashCode = (primeNumber * hashCode + key.charCodeAt(i)) % this.capacity;
     }
     return hashCode;
   }
 
   set(key, value) {
-   /* const hash = this.hash;
-    const index = hash % this.size;
-    const bucket = this.buckets[index]; */
+    const index = this.hash(key);
+
+    if (index < 0 || index >= this.buckets.length) {
+      throw new Error("Trying to access index out of bounds");
+    }
+
+    if (!this.buckets[index]) {
+      this.buckets[index] = [];
+    }
+
+    const buckets = this.buckets[index];
+    for (let i = 0; i < this.buckets.length; i++) {
+      if (buckets[i][0] === key) {
+        buckets[i][1] = value;
+        return;
+      }
+    }
+    // if key doesn't exist ==>  add new pair [key ,value] 
+    buckets.push([key, value]);
+    this.size++;
     
+    if (this.size >= this.capacity * this.loadFactor) {
+      this.resize();
+    }
+
   }
-  
+
+  resize() {
+    const oldBuckets = this.buckets;
+    this.capacity *= 2;
+    this.buckets = new Array(this.capacity);
+    this.size = 0;
+
+    for (let bucket of this.buckets) {
+      if (bucket) {
+        for (let [key, value] of bucket) {
+          this.set(key, value);
+        }
+      }
+    }
+  }
+
   get(key) {}
   has(key) {}
   remove(key) {}
@@ -31,9 +67,12 @@ class HashMap {
   values() {}
   entries() {}
 }
-/*
-const test = new HashMap();
-test.set('apple', 'banana');*/
+//buckets example :
+BUCKET = [
+    [0]        [1]
+  ["apple",   "red"  ], // FIRST element: [key, value]
+  ["orange", "orange"], // SECOND element: [key, value]
+];
 
 const testHash = new HashMap();
-console.log(testHash.hash('Andrei'));
+console.log(testHash.hash("Andrei"));
